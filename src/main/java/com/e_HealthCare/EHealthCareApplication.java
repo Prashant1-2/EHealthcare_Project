2085dd10dc3b97dd8ebb5c38256fbcaa1cb9eedb
Prashant1-2 <prashantagrawal2001@gmail.com>
2025-03-30 20:13:18 +0530

    Initial commit for EHealthCare Spring Boot project

---
+++ src/main/java/com/e_HealthCare/EHealthCareApplication.java
@@ -0,0 +1,13 @@
+package com.e_HealthCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EHealthCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EHealthCareApplication.class, args);
+	}
+
+}
