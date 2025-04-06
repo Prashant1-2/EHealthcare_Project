9d41f681835e15acfb2a2aecdf58cc32e7974730
Prashant1-2 <prashantagrawal2001@gmail.com>
2025-04-06 15:23:42 +0530

    updated

--- src/main/java/com/e_HealthCare/EHealthCareApplication.java
+++ src/main/java/com/e_HealthCare/EHealthCareApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EHealthCareApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EHealthCareApplication.class, args);
 	}
-
 }
